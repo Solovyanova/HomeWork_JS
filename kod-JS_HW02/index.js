2566cf572dc0ba5b774fbf1055fd4b4abc3dbef2
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-08 21:43:25 +0300

    task_5

--- kod-JS_HW02/index.js
+++ kod-JS_HW02/index.js
@@ -15,3 +15,7 @@ alert(a + c);
 alert(a - c);
 alert(a * c);
 alert(a / c);
+
+let up = Number(alert('Желаете узнать сколько будет 2 в 5й степени?'))
+let result = 2 ** 5;
+alert (` 2 в 5й степени будет ${result}`);
