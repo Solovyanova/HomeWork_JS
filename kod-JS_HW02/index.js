d8305e1cbb10613cfe91a705bb242645fcdb0872
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-08 23:08:38 +0300

    corrected the task_4

--- kod-JS_HW02/index.js
+++ kod-JS_HW02/index.js
@@ -3,6 +3,7 @@ alert(a);
 let b = 20;
 alert(b);
 
+
 let num;
 num = 2007;
 alert(num);
@@ -10,12 +11,29 @@ alert(num);
 let userName = 'Brendan Eyck';
 alert (userName);
 
-let с = 2;
-alert(a + c);
-alert(a - c);
-alert(a * c);
-alert(a / c);
+let c = 10;
+let d = 2;
+let plus = c+d;
+alert (plus)
+let minus = c-d;
+alert (minus)
+let mult = c*d;
+alert (mult)
+let division = c/d;
+alert (division)
 
 let up = Number(alert('Желаете узнать сколько будет 2 в 5й степени?'))
 let result = 2 ** 5;
 alert (` 2 в 5й степени будет ${result}`);
+
+alert('Желаете узнать какой будет остаток отделения 9 на 2?')
+let aa = 9;
+let bb = 2;
+let rest = aa % bb;
+if (rest === 0) {
+	console.log('делится нацело');
+}
+ else {
+	console.log('делится с остатком ' + rest);
+}
+alert(`остаток от деления 1`);
