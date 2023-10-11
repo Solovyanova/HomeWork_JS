1a03c43b726e44d386d9530e3856bb29317e4236
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-11 19:04:56 +0300

    the name of the task_9.1 has been changed to 9.0

--- kod-JS_HW02/index.js
+++ kod-JS_HW02/index.js
@@ -1,16 +1,19 @@
+//1
 let a = 10;
 alert(a);
 let b = 20;
 alert(b);
 
-
+//2
 let number;
 number = 2007;
 alert(number);
 
+//3
 let userName = 'Brendan Eyck';
 alert (userName);
 
+//4
 let c = 10;
 let d = 2;
 let plus = c+d;
@@ -22,10 +25,12 @@ alert (mult)
 let division = c/d;
 alert (division)
 
+//5
 let up = Number(alert('Желаете узнать сколько будет 2 в 5й степени?'))
 let result = 2 ** 5;
 alert (` 2 в 5й степени будет ${result}`);
 
+//6
 alert('Желаете узнать какой будет остаток отделения 9 на 2?')
 let aa = 9;
 let bb = 2;
@@ -38,6 +43,7 @@ if (rest === 0) {
 }
 alert(`остаток от деления 1`);
 
+//7
 let num = 1;
 num += 5;
 alert(num += 5);
@@ -51,9 +57,11 @@ alert(++num)
 alert(--num)
 alert(num);
 
+//8
 let age = Number(prompt('Сколько вам лет?'));
 alert(`Вам ${age} лет?`)
 
+//9.0
 const firstuser = {
 	name: 'Vasiliy',
 	age: 28,
@@ -63,3 +71,5 @@ console.log(firstuser.age); //28
 firstuser.isAdmin = true;
 console.log(firstuser['age']); //28
 firstuser['isAdmin'] = true;
+
+//9.1
