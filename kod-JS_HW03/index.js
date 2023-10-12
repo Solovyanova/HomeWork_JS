99b325d34a9e26a2bed3fe396d1426ba96eb1909
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-12 16:12:31 +0300

    task_2

--- kod-JS_HW03/index.js
+++ kod-JS_HW03/index.js
@@ -3,3 +3,11 @@ let password = String( prompt ("Введите пароль"));
 (password === 'Solovyanova')?
 console.log ('Пароль введен верно') : 
 console.log ('Пароль введен неправильно');
+
+//2
+let c = Number( prompt ("Введите число от 0 до 10"));
+if (c>0 && c<10){
+console.log ('Верно')
+} else {
+console.log ('Неверно');
+}
