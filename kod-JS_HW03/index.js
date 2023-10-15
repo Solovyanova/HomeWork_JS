63aa22fab69263442d77f3af8bb7552b93d2cc03
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-15 14:15:58 +0300

    fixed errors in tasks after viewing workshop

--- kod-JS_HW03/index.js
+++ kod-JS_HW03/index.js
@@ -1,21 +1,24 @@
 //1
-let password = String( prompt ("Введите пароль"));
-(password === 'Solovyanova')?
-console.log ('Пароль введен верно') : 
-console.log ('Пароль введен неправильно');
+let password = 'Solovyanova';
+let userInput = ( prompt ("Введите пароль"));
+if (userInput === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
 
 //2
-let c = Number( prompt ("Введите число от 0 до 10"));
-if (c>0 && c<10){
-console.log ('Верно')
+let c = 0;
+if (c > 0 && c < 10) {
+console.log ('Верно');
 } else {
 console.log ('Неверно');
 }
 
 //3
-let d = Number( prompt ("Сколько букв в вашем имени `(`введите числ от 0 до 100 `)`")); 
-let e = Number( prompt ("Сколько букв в вашей фамилии `(`введите число от 0 до 100`)`"));
-if (d>100 || e>100){
+let d = 101; 
+let e = 50;
+if (d > 100 || e > 100){
 console.log ('Верно')
 } else {
 console.log ('Неверно');
@@ -28,33 +31,27 @@ alert(Number(a) + Number(b));
 
 //5
 let monthNumber = prompt('Введите номер месяца в году и узнайте к какому сезону этот месяц принадлежит');
-
-if (monthNumber === '1'  || monthNumber === '01') {
-  console.log('Сезон зима');
-} else if (monthNumber === '2'  || monthNumber === '02') {
-  console.log('Сезон зима');
-} else if (monthNumber === '3'  || monthNumber === '03') {
-  console.log('Сезон весна');
-} else if (monthNumber === '4'  || monthNumber === '04') {
-  console.log('Сезон весна');
-} else if (monthNumber === '5'  || monthNumber === '05') {
-  console.log('Сезон весна');
-} else if (monthNumber === '6'  || monthNumber === '06') {
-  console.log('Сезон лето');
-} else if (monthNumber === '7'  || monthNumber === '07') {
-  console.log('Сезон лето');
-} else if (monthNumber === '8'  || monthNumber === '08') {
-  console.log('Сезон лето');
-} else if (monthNumber === '9'  || monthNumber === '09') {
-  console.log('Сезон осень');
-} else if (monthNumber === '10') {
-  console.log('Сезон осень');
-} else if (monthNumber === '11') {
-  console.log('Сезон осень');
-} else if (monthNumber === '12') {
-console.log('Сезон зима');
-} else {
-  console.log('Такого Сезона не существует!');
+switch (monthNumber) {
+    case 1:
+	case 2:
+	case 12:
+	    alert ("Зима");
+	    break;
+	case 3:
+	case 4:
+	case 5:
+	    alert ("Весна");
+	    break;
+	case 6:
+	case 7:
+	case 8:
+	    alert ("Лето");
+	    break;
+	case 9:
+	case 10:
+	case 11:
+	    alert ("Осень");
+	    break;
 }
 
 //6 Создайте адаптивную версию вашего сайта и добавьте эффекты наведения.
