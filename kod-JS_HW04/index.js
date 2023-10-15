489adbdb4473ce94f0788e49bbd5ad5dc8b7325b
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-15 17:23:42 +0300

    task-3

--- kod-JS_HW04/index.js
+++ kod-JS_HW04/index.js
@@ -5,11 +5,17 @@ console.log('Привет');
 i++;
 } while (i<2);
 
-for (let i = 0; i <=2; i++) {
+for (let i = 0; i <= 2; i++) {
     console.log('Привет')
 }
 
 //2
-for (let i =1; i <=5; i++) {
+for (let i =1; i <= 5; i++) {
     console.log(i)
 }
+
+//3
+for (let i = 7; i <= 22; i++) {
+    console.log(i)
+}
+
