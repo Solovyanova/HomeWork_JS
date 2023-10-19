42c643233151596f5b413652cc5146329501327f
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-19 22:26:20 +0300

    task-2

--- kod-JS_HW05/index.js
+++ kod-JS_HW05/index.js
@@ -1,3 +1,4 @@
+//1
 function min(a, b) {
     if (a < b) {
       return a;
@@ -8,3 +9,20 @@ function min(a, b) {
   
   console.log (min(8, 4))
   console.log (min(6, 6))
+
+  //2
+  let i = prompt("Введите любое число, чтобы узнать четное оно или нечетное")
+  function evenOrOdd(i) {
+    if (i % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  if (i % 2 === 0) {
+    alert(`число ${i} четное`);
+  } else {
+    alert(`число ${i} нечетное`);
+  }
+
