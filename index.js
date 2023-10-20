bf9a25511379cc29629e35610ad201120d5bb856
Solovyanova <Solovyanova@users.noreply.github.com>
2023-10-20 16:12:23 +0300

    task-8

---
+++ index.js
@@ -0,0 +1,15 @@
+let number = Number(prompt("Введите номер месяца!"));
+function date(number) {
+    if (number === 1 || number === 2 || number=== 12) {
+        return 'Зима';
+    } else if (number === 3 || number === 4 || number=== 5) {
+       return 'Весна';
+    } else if (number === 6 || number === 7 || number=== 8) {
+        return 'Лето';
+    }  else if (number === 9 || number === 10 || number=== 11) {
+        return 'Осень';
+    } else {
+        return 'Такого месяца нет:(';
+    }
+}
+alert (date(number))
