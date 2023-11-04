fc4dca3d3dff65d77eee01d5ac5eecbb7970f240
Solovyanova <Solovyanova@users.noreply.github.com>
2023-11-04 21:08:37 +0300

    task3

--- kod-JS_HW07/index.js
+++ kod-JS_HW07/index.js
@@ -4,11 +4,16 @@ str = str.toUpperCase();
 console.log(str);
 
 //2
-
-
 function filterStrings(searchStart, startStr) {
     return array.filter((item) => item.toLowerCase().startsWith(strtStr.toLowerCase()));
 }
+
 searchStart = (['Кошка', 'Кит', 'Комар', 'Носорог'],  'ко');
 searchStart = (['яблоко', 'груша', 'гриб', 'огурец'], 'гру');
 searchStart = (['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'); 
+
+//3
+let number = 32.58884;
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
