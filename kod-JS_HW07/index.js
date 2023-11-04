cfdf4e8dcd03a34f8398492466b8fb0d651f38fb
Solovyanova <Solovyanova@users.noreply.github.com>
2023-11-04 21:00:18 +0300

    task2

--- kod-JS_HW07/index.js
+++ kod-JS_HW07/index.js
@@ -2,3 +2,13 @@
 let str = "js";
 str = str.toUpperCase();
 console.log(str);
+
+//2
+
+
+function filterStrings(searchStart, startStr) {
+    return array.filter((item) => item.toLowerCase().startsWith(strtStr.toLowerCase()));
+}
+searchStart = (['Кошка', 'Кит', 'Комар', 'Носорог'],  'ко');
+searchStart = (['яблоко', 'груша', 'гриб', 'огурец'], 'гру');
+searchStart = (['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'); 
